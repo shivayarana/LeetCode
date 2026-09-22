36289ddda43c5d3d6e3b04151bc5ba405f0c066f
Shiva Yarana <shivayarana777@gmail.com>
2026-09-23 05:15:26 +0530

    Implement preorder traversal for binary tree

---
+++ BinaryTree/P144PreorderTraversal.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    List<Integer> list = new LinkedList<>();
+    public List<Integer> preorderTraversal(TreeNode root) {
+        preorder(root);
+        return list;
+        
+    }
+    private void preorder(TreeNode root){
+        if(root!=null){
+            list.add(root.val);
+            preorder(root.left);
+            preorder(root.right);
+        }
+    }
+
+    
+}
